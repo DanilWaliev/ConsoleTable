850c545c24b8d5a8f2fffa7792fcbe9cf2348323
DanilWaliev <dnlvlv39@gmail.com>
2022-11-26 16:00:13 +0500

    init

---
+++ Table.cs
@@ -0,0 +1,18 @@
+﻿namespace Table
+{
+    public class Table<T>
+    {
+        T[,] data;
+
+        public Table(int columns, int rows)
+        {
+            data = new T[columns, rows];
+        }
+
+        public T this[int index1, int index2]
+        {
+            get => data[index1, index2];
+            set => data[index1, index2] = value;
+        }
+    }
+}
