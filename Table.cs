081d75cc14dc76516288f9d49282fabbab6c55d2
DanilWaliev <dnlvlv39@gmail.com>
2022-12-06 11:29:06 +0500

    aoaoaoaoao

--- Table.cs
+++ Table.cs
@@ -4,15 +4,30 @@
     {
         T[,] data;
 
-        public Table(int columns, int rows)
+        public Table(int rows, int columns)
         {
-            data = new T[columns, rows];
+            data = new T[rows, columns];
         }
 
         public T this[int index1, int index2]
         {
             get => data[index1, index2];
             set => data[index1, index2] = value;
+        }
+
+        public void ShowFullTable()
+        {
+
+        }
+
+        public void AddColumns(int num) //num это кол-во добавляемых столбцов
+        {
+
+        }
+
+        public void AddRows(int num) //num это кол-во добавляемых строк
+        {
+
         }
     }
 
